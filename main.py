5dcca564bcf4d3bec2fec3e8ab52b668c0edc7b6
Anusha Datta <anushadatta@gmail.com>
2020-02-22 02:46:59 +0800

    Added multiple falling cats & dogs.

--- main.py
+++ main.py
@@ -33,30 +33,35 @@ playerY = 480
 playerX_change = 0 
 
 # CAT/DOG
-url = shuffle()
-animalImg = pygame.image.load(url)
+animalImg = []
+animalX = []
+animalY = []
+animalX_change = []
+animalY_change = []
+height = 0
 
-# Position animal randomly at top
-animalX = random.randint(0,736)
-animalY = 30
-animalX_change = 0 
-animalY_change = 4
+for i in range(2):
+    url = shuffle()
+    animalImg.append(pygame.image.load(url))
+
+    # Position animal randomly at top
+    animalX.append(random.randint(100,700))
+    animalY.append(height) 
+    animalX_change.append(0)  
+    animalY_change.append(3)
+
+    height += 200
 
 def player(x,y):
     # blit(image, coordinates) to print player onto game screen  
     screen.blit(playerImg, (x,y))
 
-def animal(x,y):
-    # blit(image, coordinates) to print animal onto game screen  
-    screen.blit(animalImg, (x,y))
+def animal(x,y,i):
+    # blit(image, coordinates) to print animal onto game screen 
+    screen.blit(animalImg[i], (x,y))
 
 def isCollision(animalX, animalY, playerX, playerY):
     distance = math.sqrt(math.pow(animalX-playerX, 2) + math.pow(animalY-playerY,2))
-    
-    if distance < 60:
-        return True
-    else:
-        return False 
 
 # Score 
 score_value = 0 
@@ -86,7 +91,7 @@ over_font = pygame.font.Font('freesansbold.ttf',64)
 
 def game_over_text():
     game_over = over_font.render("GAME OVER", True, (10,10,10))
-    screen.blit(game_over, (200,250))
+    screen.blit(game_over, (200,380))
 
 # Game loop (persistence of game screen)
 running = True
@@ -109,11 +114,11 @@ while running:
             
             # Left key pressed
             if event.key == pygame.K_LEFT:
-                playerX_change = -7 
+                playerX_change = -10 
 
             # Right key pressed 
             if event.key == pygame.K_RIGHT:
-                playerX_change = +7
+                playerX_change = +10
         
         # Checking keystrokes (releasing a key)
         if event.type == pygame.KEYUP:
@@ -132,36 +137,41 @@ while running:
     # Updating basket position horizontally 
     playerX += playerX_change
 
-    # Updating animal direction vertically 
-    if animalY <= 536:
-        animalY += animalY_change
+    for i in range(2):
+        # Updating animal direction vertically 
+        if animalY[i] <= 536:
+            animalY[i] += animalY_change[i]
 
-    # Collision
-    collision = isCollision(animalX, animalY, playerX, playerY)
+        # Collision
+        collision = isCollision(animalX[i], animalY[i], playerX, playerY)
 
-    if collision:
-        score_value +=1
+        if collision:
+            score_value +=1
 
-        score_sound = mixer.Sound('assets/PointScore.wav')
-        score_sound.play()
+            score_sound = mixer.Sound('assets/PointScore.wav')
+            score_sound.play()
 
-        animalX = random.randint(0,736)
-        animalY = 30
-    
-    # TIMER
-    if(animalY < 534):
-        updated_time = int(time.time() - startTime)
-
-    # GameOver
-    if animalY == 534:
-        gameover_sound = mixer.Sound('assets/GameOver.flac')
-        gameover_sound.play()
+            animalX[i] = random.randint(0,736)
+            animalY[i] = 30
         
-    if animalY >= 534:
-        game_over_text()
+        # TIMER
+        if(animalY[i] < 534):
+            updated_time = int(time.time() - startTime)
+
+        # GameOver
+        if animalY[i] == 534:
+            gameover_sound = mixer.Sound('assets/GameOver.flac')
+            gameover_sound.play()
+            
+            
+        if animalY[i] >= 534:
+            game_over_text()
+            for i in range(2):
+                    animalY[i] = 2000
+
+        animal(animalX[i],animalY[i], i)
 
     player(playerX,playerY)
-    animal(animalX,animalY)
     showScore(textX, textY)
     showTime(updated_time)
 
